2fa904674c16738e12d35b893cc3a34003e26371
Frank Celler <frank@arangodb.com>
2019-03-17 12:15:29 +0100

    cd into database before executing

--- ansible/centos7-3-node-cluster/files/libexec/arangodb-cluster.sh
+++ ansible/centos7-3-node-cluster/files/libexec/arangodb-cluster.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+set -e
+
+BASEDIR=/var/lib/arangodb3-cluster
+
+cd "$BASEDIR"
 exec /usr/bin/arangodb \
-     --starter.data-dir=/var/lib/arangodb3-cluster \
+     --starter.data-dir="$BASEDIR" \
      --log.dir=/var/log/arangodb3 \
      `cat /etc/arangodb3/cluster.conf`
